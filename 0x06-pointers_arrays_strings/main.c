ce646a0939de3b0d7d419306ead365e861737ddb
Paul Ofili <ofilipaul90@gmail.com>
2022-09-22 14:51:47 -0700

    Task10

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,22 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - calls other func.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: 0 always
  */
-int main(void)
-{
-	print_number(98);
-	_putchar('\n');
-	print_number(402);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	return (0);
-}
+
